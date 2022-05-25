4356a0fffea4136839cef826152c771f6af40cfc
Sergei Diachenko <d.sergei.dev@gmail.com>
2022-05-25 13:57:09 +0300

    modified logo & footer components, added user agreement

---
+++ coffee-store/src/app/user-agreement/user-agreement.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-userAgreement',
+    templateUrl: './user-agreement.component.html',
+    styleUrls: ['./user-agreement.component.scss']
+})
+
+export class UserAgreement {  }
