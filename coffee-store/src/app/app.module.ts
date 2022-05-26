d1717c01a99cb90ccb6aa5ffae391d72f96b8d30
Sergei Diachenko <d.sergei.dev@gmail.com>
2022-05-26 17:05:30 +0300

    added email validation in newsletter subscription

--- coffee-store/src/app/app.module.ts
+++ coffee-store/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { CatalogExplorer } from './home/catalog-explorer/catalog-explorer.component';
 import { MainTitleComponent } from './home/main-title/main-title.component';
 import { NavigationComponent } from './navigation/navigation.component';
@@ -12,6 +12,7 @@ import { Contacts } from './contacts/contacts.component';
 import { Wildcard } from './wildcard/wildcard.component';
 import { Footer } from './footer/footer.component';
 import { UserAgreement } from './user-agreement/user-agreement.component';
+
 @NgModule({
   declarations: [
     AppComponent,
